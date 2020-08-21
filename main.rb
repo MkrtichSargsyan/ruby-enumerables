fc6539eae6c0768d7b62609162dc6117a7785e12
El-Potato-Slayer <leighchad.cl@gmail.com>
2020-08-21 15:05:15 +0200

    created the my_each_with_index method

--- main.rb
+++ main.rb
@@ -2,6 +2,14 @@ module Enumerable
   def my_each
     each { |el| yield el }
   end
+
+  def my_each_with_index
+    index = 0
+    each do |el|
+      yield(el, index)
+      index += 1
+    end
+  end
 end
 
-[1, 2, 6].my_each { |el| puts el }
+[1, 2, 6].my_each_with_index { |el, index| puts el, index }
