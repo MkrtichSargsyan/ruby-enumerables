2b7643f198bbaf6afe5d117bf6150c66f1268453
El-Potato-Slayer <leighchad.cl@gmail.com>
2020-10-05 15:44:39 +0200

    added tests for my_each_with_index method"

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -8,12 +8,24 @@ describe Enumerable do
     describe "#my_each" do
       it "It returns multiplcation of the array" do
           result = []
-          new_array = array.my_each {|e| result << e * 2}
+          array.my_each {|e| result << e * 2}
           expect(result).to eq([2,6,10,14])
       end
 
       it "it should return enumerator if block is not given" do
         expect(array.my_each).to be_an Enumerator
+      end
+    end
+
+    describe "#my_each" do
+      it "returns element index" do
+          result = []
+          array.my_each_with_index {|e, index| result << index}
+          expect(result).to eq([0, 1, 2, 3])
+      end
+
+      it "it should return enumerator if block is not given" do
+        expect(array.my_each_with_index).to be_an Enumerator
       end
     end
 end
